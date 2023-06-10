3f0b2167c995769b80742c4ea093eeefdcf33bdc
Fatma Guleryuz <fglryz.111@gmail.com>
2023-06-11 00:19:45 +0100

    assertion is added

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,12 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {   
     baseUrl: 'https://practice.cydeo.com/',
+    video: false,
+    retries :2,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
     },
   },
+  
 });
--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -12,13 +12,49 @@ it('Check different locators strategiest',()=>{
     cy.get("input[name='username']").type("Cydeo Student");//every statement creates an object to be interactived, and next command makes operation to the object at the prevoious statement
     //attribute name and value 
    cy.get('[type="text"]').clear();//clear what is typed
+   //tag name
 cy.get('input').each((item, index ,list)=>{
   //assert lengtt of the list 2
   expect(list).to.have.length(2);
   expect(item).to.have.attr("type");
 });
 
+//by attribute
+cy.get('[type]');
+
+//class name
+cy.get('.btn.btn-primary');
+
+//id name
+cy.get('#wooden_spoon')
+
+//if I want to use text: no xpath in cypress,but it still possible with a different approach
+
+cy.get('button').should('contain','Login').click();
+
 
 })
+it('Check finding elements by traveling through DOM',()=>{
+  //travel to find login button:locate username box go to parent  from then find button
+
+cy.get('input[name="username"]').parents('form') .find('button').should('contain','Login').click();
+
+})
+it("Check diffrent type of assertions", () => {
+  
+//cypress itself bundles assertions provided Chai,Sinon and JQuery library
+
+  //Should assertion: does assertion directly on the object itself
+  cy.get('#wooden_spoon')
+  .should('contain','Login').and('have.class','btn btn-primary');
+  cy.get("#wooden_spoon").then((buttonElement)=>{
+    expext(buttonElement).to.have.text('Login');
+    expext(buttonElement).to.have.class('btn btn-primary');
+
+
+  });
+
+    
+});
 
 })
